d0840e82056e48688ba908c775a1cefa3f10fd0e
Deny <deniphoto@mail.ru>
2023-10-21 10:28:39 +0300

    phpLesson

---
+++ first_Lesson.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 100;
+$b = 25;
+
+$c = $a + $b; 
+echo "$c  </br>";
+
+$d = $a - $b;
+echo "$d  </br>";
+
+$e = $a * $b;
+echo "$e  </br>";
+
+$f = $a / $b;
+echo "$f  </br>";
+
+
+$name = "Deny";
+
+$age = 21;
+
+echo "Hello $name yuor age is $age";
